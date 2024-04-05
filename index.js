da0a889e1a8ac997169fe440e826ab0f56ce560d
Aminul667 <rahathssn@gmail.com>
2024-04-06 02:18:21 +0600

    added delete function to the bookmark data

--- index.js
+++ index.js
@@ -181,6 +181,14 @@ async function run() {
       res.send(result);
     });
 
+    // delete a bookmarked article
+    app.delete("/bookmark/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await bookmarksCollection.deleteOne(query);
+      res.send(result);
+    });
+
     // end of the backend
 
     // Send a ping to confirm a successful connection
